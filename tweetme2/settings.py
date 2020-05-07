407b333abf094791c2630c819850f52d95497fae
kasra02 <Kasra.niroumand@gmail.com>
2020-05-07 10:44:47 +0430

    second commit

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+f
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
